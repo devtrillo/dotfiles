a228a61de1d9130891f280e377fd4aafde1b30e2
Esteban Trillo <esteban@devtrillo.com>
2021-08-02 07:52:11 -0500

    New Config

--- config/nvim/lv-config.lua
+++ config/nvim/lv-config.lua
@@ -1,84 +1,97 @@
---[[
-O is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+
 -- general
-O.format_on_save = true
-O.auto_complete = true
-O.colorscheme = "gruvbox"
-O.auto_close_tree = 1
-O.wrap_lines = false
-O.timeoutlen = 100
-O.leader_key = " "
-O.ignore_case = true
-O.smart_case = true
+lvim.format_on_save = false
+lvim.lint_on_save = false
+lvim.colorscheme = "gruvbox"
 
--- TODO User Config for predefined plugins
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-O.plugin.zen.active = true
-O.plugin.dashboard.active = true
-O.plugin.telescope.active = true
-O.plugin.floatterm.active = true
--- if you don't want all the parsers change this to a table of the ones you want
-O.treesitter.ensure_installed = "all"
-O.treesitter.ignore_install = { "haskell" }
-O.treesitter.highlight.enabled = true
+-- keymappings
+lvim.leader = "space"
 
--- python
-O.lang.python.isort = true
-O.lang.python.diagnostics.virtual_text = true
-O.lang.python.analysis.use_library_code_types = true
-
--- javascript
-O.lang.tsserver.linter = 'eslint'
-
-
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
-O.user_autocommands = {{ "BufWinEnter", "*", "highlight Normal guibg=none"}}
-
--- Additional Leader bindings for WhichKey
-O.user_which_key = {
-  ["."] = "LunarConfig",
-  t={
-    name="+Trouble",
-    t={"<cmd> :TroubleToggle<cr>","Trouble Toggle"}
-  },
-  u={
-    name="+Utils",
-    b={"<cmd> lua require('lv-macos').background_selector()<cr>", "Change background"},
-  },
+-- overwrite the key-mappings provided by LunarVim for any mode, or leave it empty to keep them
+lvim.keys.normal_mode = {
+    --  Page down/up
+    {"[d", "<PageUp>"},
+    {"]d", "<PageDown>"},
+    --
+    -- Navigate buffers
+    {"<M-Right>", ":BufferNext<CR>"},
+    {"<M-Left>", ":BufferPrevious<CR>"},
+    {"<M-Down>", ":BufferClose<CR>"}
 }
+-- if you just want to augment the existing ones then use the utility function
+-- require("utils").add_keymap_insert_mode({ silent = true }, {
+-- { "<C-s>", ":w<cr>" },
+-- { "<C-c>", "<ESC>" },
+-- })
+-- you can also use the native vim way directly
+--vim.api.nvim_set_keymap("i", "<C-Space>", "compe#complete()", { noremap = true, silent = true, expr = true })
+
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.dashboard.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 0
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {}
+lvim.builtin.treesitter.ignore_install = {"haskell"}
+lvim.builtin.treesitter.highlight.enabled = true
+
+-- generic LSP settings
+-- you can set a custom on_attach function that will be used for all the language servers
+-- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
 
 -- Additional Plugins
-O.user_plugins = {
-  {"styled-components/vim-styled-components"},
-  {"morhetz/gruvbox"},
-  {"tpope/vim-surround"},
-  {"wakatime/vim-wakatime", event = 'VimEnter'},
-  {"tpope/vim-eunuch"},
-  {"ap/vim-css-color"},
-  {"mattn/emmet-vim", opt = true, ft={"tsx","js","jsx","svelte","html"}},
-
- {
-    "folke/trouble.nvim",
-    cmd = "TroubleToggle"}
+lvim.plugins = {
+    {"styled-components/vim-styled-components"},
+    {"morhetz/gruvbox"},
+    {"tpope/vim-surround"},
+    {"wakatime/vim-wakatime", event = "VimEnter"},
+    {"tpope/vim-eunuch"},
+    {"ap/vim-css-color"},
+    {"mattn/emmet-vim", opt = true, ft = {"tsx", "js", "jsx", "svelte", "html"}},
+    {"folke/trouble.nvim", cmd = "TroubleToggle"}
 }
 
-vim.api.nvim_set_keymap("n", "<M-Right>", ":BufferNext<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<M-Left>", ":BufferPrevious<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<M-Down>", ":BufferClose<CR>", { noremap = true, silent = true })
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- lvim.autocommands.custom_groups = {
+--   { "BufWinEnter", "*", "highlight Normal guibg=none"},
+-- }
+vim.api.nvim_command([[
+  autocmd BufEnter * :highlight Normal guibg=none
+]])
+-- Additional Leader bindings for WhichKey
+lvim.builtin.dashboard.custom_header = {
+    "██████╗ ███████╗██╗   ██╗    ██╗   ██╗██╗███╗   ███╗",
+    "██╔══██╗██╔════╝██║   ██║    ██║   ██║██║████╗ ████║",
+    "██║  ██║█████╗  ██║   ██║    ██║   ██║██║██╔████╔██║",
+    "██║  ██║██╔══╝  ╚██╗ ██╔╝    ╚██╗ ██╔╝██║██║╚██╔╝██║",
+    "██████╔╝███████╗ ╚████╔╝      ╚████╔╝ ██║██║ ╚═╝ ██║",
+    "╚═════╝ ╚══════╝  ╚═══╝        ╚═══╝  ╚═╝╚═╝     ╚═╝"
+}
 
-vim.cmd 'highlight Normal guibg=none'
--- dashboard
-O.plugin.dashboard.custom_header = {
-  '██████╗ ███████╗██╗   ██╗    ██╗   ██╗██╗███╗   ███╗',
-  '██╔══██╗██╔════╝██║   ██║    ██║   ██║██║████╗ ████║',
-  '██║  ██║█████╗  ██║   ██║    ██║   ██║██║██╔████╔██║',
-  '██║  ██║██╔══╝  ╚██╗ ██╔╝    ╚██╗ ██╔╝██║██║╚██╔╝██║',
-  '██████╔╝███████╗ ╚████╔╝      ╚████╔╝ ██║██║ ╚═╝ ██║',
-  '╚═════╝ ╚══════╝  ╚═══╝        ╚═══╝  ╚═╝╚═╝     ╚═╝',
+vim.cmd "highlight Normal guibg=none"
+
+lvim.builtin.which_key.mappings["u"] = {
+    name = "+Utils",
+    b = {"<cmd> lua require('personal').background_selector()<cr>", "Change background"}
+}
+lvim.builtin.which_key.mappings["t"] = {
+    name = "+Trouble",
+    r = {"<cmd>Trouble lsp_references<cr>", "References"},
+    f = {"<cmd>Trouble lsp_definitions<cr>", "Definitions"},
+    d = {"<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnosticss"},
+    q = {"<cmd>Trouble quickfix<cr>", "QuickFix"},
+    l = {"<cmd>Trouble loclist<cr>", "LocationList"},
+    w = {"<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnosticss"},
+    t = {"<cmd> :TroubleToggle<cr>", "Trouble Toggle"}
+}
