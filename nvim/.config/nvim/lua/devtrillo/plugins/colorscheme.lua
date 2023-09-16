11cd7bf3af3a7f40b84e03ad5251bc69dbc18480
Esteban Trillo <esteban@devtrillo.com>
2023-09-16 15:51:15 -0600

    feat: add multiple colorschemes

--- nvim/.config/nvim/lua/devtrillo/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/devtrillo/plugins/colorscheme.lua
@@ -1,15 +1,34 @@
+local theme = "onedark"
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-     style = "storm",
-     transparent = true,
-    },
-    config= function()
-      vim.cmd([[colorscheme tokyonight-storm]])
-    end
-    
-  }
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		enabled = theme == "tokyonight",
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme tokyonight-night]])
+		end,
+	},
+	{
+		"navarasu/onedark.nvim",
+		lazy = false,
+		enabled = theme == "onedark",
+		priority = 1000,
+		opts = {
+			style = "warmer",
+		},
+		config = function()
+			vim.cmd([[colorscheme onedark]])
+		end,
+	},
+	{
+		"LunarVim/lunar.nvim",
+		lazy = false,
+		enabled = theme == "lunar",
+		priority = 1000,
+		opts = {},
+		config = function()
+			vim.cmd([[colorscheme lunar]])
+		end,
+	},
 }
