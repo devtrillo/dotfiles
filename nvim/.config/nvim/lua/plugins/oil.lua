8419033be3669a17d0df95b7b374c0c9bde71914
Esteban Trillo <esteban@devtrillo.com>
2024-11-25 15:05:23 -0600

    wip

---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,37 @@
+return {
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      view_options = {
+        show_hidden = true,
+      },
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+        ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+        ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-l>"] = "actions.refresh",
+        ["e"] = "actions.parent",
+        ["E"] = "actions.open_cwd",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory", mode = "n" },
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      },
+    },
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    keys = {
+      { "<leader>e", "<cmd>Oil<CR>", desc = "Open oil" },
+    },
+  },
+}
